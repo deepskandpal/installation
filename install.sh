552a7b6c830b7784f8ad11d8c3cf7f66f64a4cc7
Deepanshu Kandpal <dkandpalz@gmail.com>
2018-02-19 12:04:20 +0530

    second commit

--- install.sh
+++ install.sh
@@ -7,5 +7,8 @@ else
  #Update and Upgrade
  echo "Updating and Upgrading"
  apt-get update && sudo apt-get upgrade -y
-
 fi
+ #Install Nodejs
+ echo "Installing Nodejs"
+ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+ apt install -y nodejs
