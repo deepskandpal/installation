8ce66fdcd90a934dc353ba3fa6f82dfbb631ee59
Deepanshu Kandpal <dkandpalz@gmail.com>
2018-02-19 12:28:06 +0530

    fifth commit

--- install.sh
+++ install.sh
@@ -26,3 +26,31 @@ apt-fast install git
 #updatin and  upgrading using apt-fast
 echo "updating and upgrading"
 apt-fast   update && apt-fast  upgrade
+
+#Tmux installation and configuration
+echo "Installing Tmux"
+sudo apt update
+
+sudo apt install -y git
+
+sudo apt install -y automake
+sudo apt install -y build-essential
+sudo apt install -y pkg-config
+sudo apt install -y libevent-dev
+sudo apt install -y libncurses5-dev
+
+rm -fr /tmp/tmux
+
+git clone https://github.com/tmux/tmux.git /tmp/tmux
+
+cd /tmp/tmux
+
+sh autogen.sh
+
+./configure && make
+
+sudo make install
+
+cd -
+
+rm -fr /tmp/tmux
